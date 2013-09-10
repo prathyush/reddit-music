a8e43e70db24d1cc2bf2654ac246abee7e1e7fb8
Prathyush Thota <prathyushthota@gmail.com>
2013-09-10 23:16:54 +0530

    Vimeo support.

--- vidlist/views.py
+++ vidlist/views.py
@@ -19,28 +19,33 @@ def index(request):
 
 def player(request):
 	subreddits = []
-	response_list = []
-	if not request.POST:
-		subreddit = request.GET.get('s','jazz')
-		limit = request.GET.get('l', '20')
-		subreddits.append(subreddit)
-		playlist = getvideos(subreddits, int(limit))
-		template = loader.get_template('vidlist/player.html')
-		context = RequestContext(request, {
-			'playlist': playlist,
-		})
-		return HttpResponse(template.render(context))
-	print "request POST"
 	response_dict = {}
+	if not request.POST:
+		subreddit = request.GET.get('s')
+		template = loader.get_template('vidlist/player.html')
+		if subreddit is not None:
+			#Some one is messing around.
+			print "Really? subreddit: "+subreddit
+			limit = request.GET.get('l', '20')
+			subreddits.append(subreddit)
+			playlist = getvideos(subreddits, int(limit))
+			#Convert into JSON. 
+			for song in playlist:
+				response_dict[song.videoid] = {'title' : song.title, 'source' : song.source, 'url' : song.url}
+			response_json = simplejson.dumps(response_dict)
+			context = RequestContext(request, {
+				'playlist': response_json,
+				'subreddit': subreddit,
+			})
+		else:
+			print "Usual Execution"
+			context= RequestContext(request, {})
+		return HttpResponse(template.render(context))
+
 	subreddit = request.POST['subreddit'].split('/')[2]
 	limit = request.POST['limit']
 	subreddits.append(subreddit)
 	playlist = getvideos(subreddits, int(limit))
-	#Convert into
-	for song in playlist:
-		response_dict[song.videoid] = {'title' : song.title, 'source' : song.source, 'url' : song.url}
-
-	response_json = simplejson.dumps(response_dict)
 	template = loader.get_template('vidlist/playlist.html')
 	context = RequestContext(request, {'songs' : playlist})
 #	return TemplateResponse(request, template, {'songs' : playlist})
